a39abe1ec6160577b333d75619e1f4dc944fd3b4
nathan <nroark@trideum.com>
2023-05-29 09:58:51 -0500

    added analytics, cleaned up some styling

--- src/components/pokedexCard.tsx
+++ src/components/pokedexCard.tsx
@@ -12,14 +12,14 @@ const PokedexCard: React.FC<cardProps> = (props) => {
         <>
             {data &&
                 < Link
-                    className={`flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 ${getTypeColorBorder(data.type1)}
+                    className={`flex flex-col max-w-xs gap-4 p-4 rounded-xl bg-white/10 ${getTypeColorBorder(data.type1)}
                      text-white hover:border-2`}
                     href={"https://www.pokemon.com/us/pokedex/" + data.name}
                     target="_blank">
-                    <h3 className="text-2xl font-bold capitalize">
-                        {data.name}
-                        <p className="text-opacity-50 text-white">#{props.index}</p>
-                    </h3>
+                    <div className="text-2xl font-bold capitalize">
+                        <h3>{data.name}</h3>
+                        <h4 className="text-opacity-50 text-white">#{props.index}</h4>
+                    </div>
                     <Image
                         src={data.spriteUrl}
                         alt=""
@@ -27,13 +27,9 @@ const PokedexCard: React.FC<cardProps> = (props) => {
                         height={256}
                         className="animate-fade-in"
                     />
-                    <div className="text-lg flex space-x-2">
-                        <div className={`font-bold rounded-sm capitalize ${getTypeColorText(data.type1)}`}>
-                            {data.type1}
-                        </div>
-                        <div className={`font-bold rounded-sm capitalize ${getTypeColorText(data.type2)}`}>
-                            {data.type2}
-                        </div>
+                    <div className="text-lg flex space-x-2 font-bold rounded-sm capitalize">
+                        <div className={`${getTypeColorText(data.type1)}`}>{data.type1}</div>
+                        <div className={`${getTypeColorText(data.type2)}`}>{data.type2}</div>
                     </div>
                 </Link >
             }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import PokedexCard from "~/components/pokedexCard";
+import Pokeball from '~/images/pokeball.png'
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   const pokedexNumbers = 251
@@ -9,23 +11,31 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Pokédex</title>
+        <meta property="" content="Pokedex App in NextJS/tRPC/Prisma/Tailwind by Nathan Roark" />
         <meta name="description" content="Pokedex App in NextJS/tRPC/Prisma/Tailwind by Nathan Roark" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#242424] to-[#141414]">
+      <main className=" flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#141414] to-[#242424]">
         <div className="container flex flex-col items-center justify-center gap-12 px-3 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            <span className="text-[hsl(0,100%,65%)]">Poké</span>dex
-          </h1>
+          <div className="flex flex-row">
+            <Image
+              src={Pokeball}
+              alt=""
+              width={128}
+              height={128}
+              className="pr-8"
+            />
+            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+              <span className="text-[#eb312e]">Poké</span>dex
+            </h1>
+          </div>
+
           <div className="grid row-auto gap-4 md:gap-8
           grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
             {[...Array(pokedexNumbers).keys()].map((_, i) =>
               <PokedexCard index={i + 1} key={"pokemon-card-" + i.toString()} />
             )}
           </div>
-          <p className="text-2xl font-thin text-white">
-            Built by Nathan Roark
-          </p>
         </div>
       </main>
     </>
